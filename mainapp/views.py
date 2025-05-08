d9643a8cdf1781b296a9ad42d9ee5ccd83bf1afd
Netr1x264 <zvlad0104@gmail.com>
2025-05-08 18:28:41 +0500

    lesson_06: data

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 
+from mainapp.models import Product
+
+
 def index(request):
     title = 'Главная страница'
 
+    prods = Product.objects.all()[:4]
+
     context = {
         'title': title,
+        'products': prods,
     }
+
+
 
     return render(request, 'index.html', context)
 
 def contact(request):
+    title = 'Контакты'
+    context = {
+        'title': title,
+    }
     return render(request, 'contact.html')
